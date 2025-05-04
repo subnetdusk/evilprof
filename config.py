29747fe4db665d61b0f9c5f7654ee78d68d41742
subnetdusk <154432116+subnetdusk@users.noreply.github.com>
2025-05-04 18:22:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 # config.py
 
+# Valore di default per il numero di copie della verifica da generare
 DEFAULT_NUM_TESTS = 30
+
+# Path delle immagini di esempio (relativo allo script app.py)
 EXAMPLE_IMAGE_PATH = "excel_example.jpg"
+ANALYSIS_IMAGE_PATH = "analisi.jpg" # <-- NUOVA COSTANTE
